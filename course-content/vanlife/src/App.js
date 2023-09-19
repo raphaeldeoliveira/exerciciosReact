8b7c15672a02408a0211b86f6644fa0f1bc98bde
raphaeldeoliveira <rafaolive3003@gmail.com>
2023-09-19 17:17:14 -0300

    primeiro projeto feito para comprender os conceitos de rotas e navegação entre as paginas no SPA

---
+++ course-content/vanlife/src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={}/>
+        <Route />
+        <Route />
+      </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
---
+++ my-project/primeiros-passos-com-router/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
+import About from "./About";
+import Home from "./Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
