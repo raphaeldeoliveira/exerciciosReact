02f3bd6213c2fa7030a73b147ba2b15724abc848
raphaeldeoliveira <rafaolive3003@gmail.com>
2023-09-20 20:32:52 -0300

    inicio da criacao da api que vai fornecer os dados para serem exibidos no site

---
+++ course-content/api-vanlife/src/main/java/br/com/teste/DataVans.java
@@ -0,0 +1,42 @@
+package br.com.teste;
+
+public class DataVans {
+
+	private String vanName;
+	private float vanPrice;
+	private String typeLocation;
+	private String vanCategory;
+	private String vanUrlImage;
+	
+	public String getVanName() {
+		return vanName;
+	}
+	public void setVanName(String vanName) {
+		this.vanName = vanName;
+	}
+	public float getVanPrice() {
+		return vanPrice;
+	}
+	public void setVanPrice(float vanPrice) {
+		this.vanPrice = vanPrice;
+	}
+	public String getTypeLocation() {
+		return typeLocation;
+	}
+	public void setTypeLocation(String typeLocation) {
+		this.typeLocation = typeLocation;
+	}
+	public String getVanCategory() {
+		return vanCategory;
+	}
+	public void setVanCategory(String vanCategory) {
+		this.vanCategory = vanCategory;
+	}
+	public String getVanUrlImage() {
+		return vanUrlImage;
+	}
+	public void setVanUrlImage(String vanUrlImage) {
+		this.vanUrlImage = vanUrlImage;
+	}
+	
+}
---
+++ course-content/api-vanlife/src/test/java/br/com/teste/ApiVanlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVanlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
