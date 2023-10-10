e7e4606b26efe47dd38137fc4d0b8f6294ba647f
raphaeldeoliveira <rafaolive3003@gmail.com>
2023-10-10 11:42:44 -0300

    implementei toda a interface do dashboard (estilos) e comecei a implementar a secção income

--- course-content/vanlife/src/pages/host/Dashboard.js
+++ course-content/vanlife/src/pages/host/Dashboard.js
@@ -7,14 +7,14 @@ export default function Dashboard() {
 
     function Card(props) {
         return (
-            <div>
+            <div className="listed-van">
                 <img src={props.thumbImage}/>
                 <div>
-                    <h4>{props.title}</h4>
-                    <h5>{props.status}</h5>
-                </div>
-                <div>
-                    <h5>delete</h5>
+                    <div>
+                        <h4>{props.title}</h4>
+                        <h5 className="price-by-time">{props.status}</h5>
+                    </div>
+                    <div className="edit">Edit</div>
                 </div>
             </div>
         )
@@ -30,23 +30,17 @@ export default function Dashboard() {
 
     return (
 
-        <>
+        /*<>
             <h1>Dashboard goes here</h1>
-            {/*<Outlet />*/}
-        </>
+            *<Outlet /> // esse outlet nao foi implementado
+        </>*/
 
-        /*<div className="host">
-            <nav>
-                <Link to="" >Dashboard</Link>
-                <Link to="" >Income</Link>
-                <Link to="" >Vans</Link>
-                <Link to="" >Reviews</Link>
-            </nav>
+        <div className="host">
             <div className="colorful-section">
                 <div className="light-yellow">
                     <div>
                         <h3>Welcome!</h3>
-                        <h5>Total cash be payed</h5>
+                        <h5>Income last <span className="day-type">30 days</span></h5>
                         <h2>$2,260</h2>
                     </div>
                     <div>Detail</div>
@@ -54,24 +48,41 @@ export default function Dashboard() {
                 <div className="dark-yellow">
                     <div>
                         <h3>Review Score</h3>
-                        <h4>4.8/5</h4>  
+                        <h4><span className="star">&#9733;</span> 4.8<span className="rating-of">/5</span></h4>  
                     </div>
                     <div>
-                        <h5>detail</h5>
+                        <div>Detail</div>
                     </div>
                 </div>
             </div>
-            <div>
-                <div>
+            <div className="listed-vans">
+                <div className="subtitles">
                     <h4>Your listed vans</h4>
                     <h5>View all</h5>
                 </div>
                 <div>
                     {loadComponents}
-                    <Card />
+                    <Card 
+                        // teste deste componente
+                        thumbImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiCGw2tf4gLYRNmz-__GCyIO1wLn8WcoGNrFDZd-vvvy967GnKARvpCpa5D7iN_JxQwZI&usqp=CAU"
+                        title="Modest Explorer"
+                        status="60$/day"
+                    />
+                    <Card 
+                        // teste deste componente
+                        thumbImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiCGw2tf4gLYRNmz-__GCyIO1wLn8WcoGNrFDZd-vvvy967GnKARvpCpa5D7iN_JxQwZI&usqp=CAU"
+                        title="Modest Explorer"
+                        status="60$/day"
+                    />
+                    <Card 
+                        // teste deste componente
+                        thumbImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiCGw2tf4gLYRNmz-__GCyIO1wLn8WcoGNrFDZd-vvvy967GnKARvpCpa5D7iN_JxQwZI&usqp=CAU"
+                        title="Modest Explorer"
+                        status="60$/day"
+                    />
                 </div>
             </div>
-        </div>*/
+        </div>
     )
 
 }
